c1aa1d9fe9cef594f28923070ef3963b2948c16f
katarem <crismigesp@gmail.com>
2023-02-14 14:13:18 +0000

    holaMundo

--- src/main/java/dad/LoLstats/App.java
+++ src/main/java/dad/LoLstats/App.java
@@ -1,13 +1,19 @@
 package dad.LoLstats;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+public class App extends Application{
+
+    @Override public void start(Stage stage) throws Exception {
+        LoginController l = new LoginController();
+        stage.setTitle("LoLstats v1.0");
+        stage.setScene(new Scene(l.getView()));
+        stage.show();
+
+    }
+    public static void main(String[] args){
+        launch(args);
     }
 }
--- src/main/java/dad/LoLstats/Main.java
+++ src/main/java/dad/LoLstats/Main.java
@@ -1,2 +1,9 @@
-package dad.LoLstats;public class Main {
+package dad.LoLstats;
+
+public class Main {
+
+    public static void main(String[] args) {
+        App.main(args);
+    }
+
 }
