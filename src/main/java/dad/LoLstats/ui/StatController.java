e2d4be98b090baa8c8f998557be978df88ba9d1a
fvarrui <4354486+fvarrui@users.noreply.github.com>
2023-03-02 21:18:10 +0000

    problema con informes resuelto

--- src/main/java/dad/LoLstats/ui/StatController.java
+++ src/main/java/dad/LoLstats/ui/StatController.java
@@ -40,293 +40,304 @@ import net.sf.jasperreports.engine.JasperExportManager;
 import net.sf.jasperreports.engine.JasperFillManager;
 import net.sf.jasperreports.engine.JasperPrint;
 import net.sf.jasperreports.engine.JasperReport;
-import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
 
-public class StatController implements Initializable{
+public class StatController implements Initializable {
 
-    @FXML private BorderPane view;
+	@FXML
+	private BorderPane view;
 
-    @FXML private ImageView profilePicView, eloView;
+	@FXML
+	private ImageView profilePicView, eloView;
 
-    @FXML private Label playerNameLabel, playerLevelLabel, eloLabel, winrateLabel;
+	@FXML
+	private Label playerNameLabel, playerLevelLabel, eloLabel, winrateLabel;
 
-    @FXML private PieChart winrateChart;
+	@FXML
+	private PieChart winrateChart;
 
-    @FXML private ListView<GridPane> gamesView;
+	@FXML
+	private ListView<GridPane> gamesView;
 
-    @FXML private Button pdfButton, calcButton, logoutButton;
+	@FXML
+	private Button pdfButton, calcButton, logoutButton;
 
-    private Summoner summoner;
+	private Summoner summoner;
 
-    private ArrayList<LeagueEntry> elos;
+	private ArrayList<LeagueEntry> elos;
 
-    private LeagueEntry rankedEntry;
+	private LeagueEntry rankedEntry;
 
-    private String region;
+	private String region;
 
-    public static String eloValue;
+	public static String eloValue;
 
-    private ArrayList<GameController> gList;
+	private ArrayList<GameController> gList;
 
-    public StatController(ArrayList<LeagueEntry> elos, String region){
-        try{
-            this.elos = elos;
-            summoner = App.summoner;
-            getRegion(region);
-            FXMLLoader l = new FXMLLoader(getClass().getResource("/fxml/statView.fxml"));
-            l.setController(this);
-            l.load();
-        } catch (IOException e){
-            e.printStackTrace();
-        }
-    }
+	public StatController(ArrayList<LeagueEntry> elos, String region) {
+		try {
+			this.elos = elos;
+			summoner = App.summoner;
+			getRegion(region);
+			FXMLLoader l = new FXMLLoader(getClass().getResource("/fxml/statView.fxml"));
+			l.setController(this);
+			l.load();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
-    private void getRegion(String reg) {
+	private void getRegion(String reg) {
 
-        switch (reg) {
-            case "euw1":
-                region = "europe";
-                break;
-            case "eun1":
-                region = "europe";
-                break;
-            case "ru1":
-                region = "europe";
-                break;
-            case "tr1":
-                region = "europe";
-                break;
-            case "br1":
-                region = "americas";
-                break;
+		switch (reg) {
+		case "euw1":
+			region = "europe";
+			break;
+		case "eun1":
+			region = "europe";
+			break;
+		case "ru1":
+			region = "europe";
+			break;
+		case "tr1":
+			region = "europe";
+			break;
+		case "br1":
+			region = "americas";
+			break;
 
-            case "na1":
-                region = "americas";
-                break;
-            case "la1":
-                region = "americas";
-                break;
-            case "la2":
-                region = "americas";
-                break;
-            case "jp1":
-                region = "asia";
-                break;
-            case "kr":
-                region = "asia";
-                break;
-            case "oce":
-                region = "sea";
-                break;
+		case "na1":
+			region = "americas";
+			break;
+		case "la1":
+			region = "americas";
+			break;
+		case "la2":
+			region = "americas";
+			break;
+		case "jp1":
+			region = "asia";
+			break;
+		case "kr":
+			region = "asia";
+			break;
+		case "oce":
+			region = "sea";
+			break;
 
-            default:
-                region = "europe";
-                break;
-        }
+		default:
+			region = "europe";
+			break;
+		}
 
+	}
 
-    }
+	public BorderPane getView() {
+		return view;
+	}
 
-    public BorderPane getView(){
-        return view;
-    }
+	public void initialize(URL location, ResourceBundle resources) {
+		view.setCursor(new ImageCursor(
+				new Image(getClass().getResourceAsStream("/cursors/normal.png"), 128, 128, true, true)));
 
-    public void initialize(URL location, ResourceBundle resources){
-        view.setCursor(new ImageCursor(new Image(getClass().getResourceAsStream("/cursors/normal.png"),128,128,true,true)));
+		App.stage.setResizable(true);
+		ImageView calcIcon = new ImageView(new Image(getClass().getResourceAsStream("/images/calc-icon.png")));
+		calcIcon.setFitHeight(60);
+		calcIcon.setFitWidth(60);
 
-        App.stage.setResizable(true);
-        ImageView calcIcon = new ImageView(new Image(getClass().getResourceAsStream("/images/calc-icon.png")));
-        calcIcon.setFitHeight(60);
-        calcIcon.setFitWidth(60);
+		ImageView pdfIcon = new ImageView(new Image(getClass().getResourceAsStream("/images/pdf-icon.png")));
+		pdfIcon.setFitHeight(60);
+		pdfIcon.setFitWidth(60);
 
-        ImageView pdfIcon = new ImageView(new Image(getClass().getResourceAsStream("/images/pdf-icon.png")));
-        pdfIcon.setFitHeight(60);
-        pdfIcon.setFitWidth(60);
+		calcButton.setGraphic(calcIcon);
+		pdfButton.setGraphic(pdfIcon);
+		pdfButton.setOnAction(e -> {
+			try {
+				pdfOnClick();
+			} catch (JRException | IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+		});
 
-        calcButton.setGraphic(calcIcon);
-        pdfButton.setGraphic(pdfIcon);
-        pdfButton.setOnAction(e -> {
-            try {
-                pdfOnClick();
-            } catch (JRException | IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
-        });
+		logoutButton.setGraphic(new ImageView(
+				new Image(getClass().getResourceAsStream("/images/logout-icon.png"), 32, 32, true, true)));
 
-        logoutButton.setGraphic(new ImageView(new Image(getClass().getResourceAsStream("/images/logout-icon.png"),32,32,true,true)));
+		profilePicView.setImage(new Image(getClass()
+				.getResourceAsStream(String.format("/assets/profileicon/%s.png", summoner.getProfileIconId()))));
 
+		playerNameLabel.setText(summoner.getName());
+		playerLevelLabel.setText("Level " + summoner.getSummonerLevel());
 
-        profilePicView.setImage(new Image(getClass().getResourceAsStream(String.format("/assets/profileicon/%s.png",summoner.getProfileIconId()))));
+		if (elos.size() > 0) {
+			int cont = 0;
+			for (int i = 0; i < elos.size(); i++) {
+				if (elos.get(i).getQueueType().equals("RANKED_SOLO_5x5")) {
+					cont = i;
+					break;
+				}
+			}
 
-            playerNameLabel.setText(summoner.getName());
-            playerLevelLabel.setText("Level " + summoner.getSummonerLevel());
-            
-            if(elos.size()>0){
-                int cont = 0;
-                for (int i = 0; i<elos.size();i++) {
-                    if(elos.get(i).getQueueType().equals("RANKED_SOLO_5x5")){
-                        cont = i;
-                        break;
-                    }
-                }
-    
-                rankedEntry = elos.get(cont);
-                eloValue = rankedEntry.getTier() + " " + rankedEntry.getRank() + " " + rankedEntry.getLeaguePoints() + "LP";
-                eloLabel.setText(eloValue);
-                eloView.setImage(new Image(getClass().getResourceAsStream(String.format("/images/%s.png", rankedEntry.getTier().toLowerCase()))));
-        
-                ObservableList<PieChart.Data> piechartData = FXCollections.observableArrayList(
-                    new PieChart.Data("LOSERATE", getWinrate(rankedEntry.getLosses(), rankedEntry.getWins())),
-                    new PieChart.Data("a", 0),
-                    new PieChart.Data("a", 0),
-                    new PieChart.Data("WINRATE", getWinrate(rankedEntry.getWins(), rankedEntry.getLosses())), 
-                    new PieChart.Data("a", 0),
-                    new PieChart.Data("a", 0)
-                );
-    
-                winrateChart.setData(piechartData);
-                winrateChart.setLegendVisible(false);
-               
-                winrateLabel.setText(String.format("%sW %sL %.2f%%W/R",rankedEntry.getWins(),rankedEntry.getLosses(),getWinrate(rankedEntry.getWins(), rankedEntry.getLosses())));                
-                    
-            } else{
-                
-                winrateLabel.setText("");
-                eloLabel.setText("UNRANKED");
-            
-            }
+			rankedEntry = elos.get(cont);
+			eloValue = rankedEntry.getTier() + " " + rankedEntry.getRank() + " " + rankedEntry.getLeaguePoints() + "LP";
+			eloLabel.setText(eloValue);
+			eloView.setImage(new Image(getClass()
+					.getResourceAsStream(String.format("/images/%s.png", rankedEntry.getTier().toLowerCase()))));
 
-            Task<Void> task = new Task<Void>() {
-                @Override
-                protected Void call() throws Exception {
-                    
-                    ArrayList<GridPane> partidas = new ArrayList<>();
-                    gList = new ArrayList<>();
-            
-                try {
-                        
-                    MatchService mService = new MatchService(App.API_KEY, region);
-                            
-                    ArrayList<String> gameIds = mService.getGames(summoner.getPuuid(), App.API_KEY);
-                    
-                    for (String s : gameIds) {
-                                GameController g = new GameController(mService.getGame(s));
-                                if(g.getWin())
-                                    g.getView().setId("gameWon");
-                                else
-                                    g.getView().setId("gameLost");
-                                gList.add(g);
-                                partidas.add(g.getView());
-                    }
-                } catch (Exception e) {            
-                    e.printStackTrace();
-                }
-            
-            ObservableList<GridPane> partidasToList = FXCollections.observableArrayList(partidas);
-            gamesView.getSelectionModel().setSelectionMode(null);
-            gamesView.setItems(partidasToList);
-            gamesView.setPadding(new Insets(0, 0, 0, 0));
-            gamesView.setFocusModel(null);
-            gamesView.setBorder(Border.EMPTY);
-            gamesView.setFocusTraversable(false);
-            gamesView.setCellFactory(new Callback<ListView<GridPane>,ListCell<GridPane>>() {
-    
-                @Override
-                public ListCell<GridPane> call(ListView<GridPane> param) {
-                    return new ListCell<>(){
-                        @Override
-                        public void updateItem(GridPane game, boolean empty) {
-                            super.updateItem(game, empty);
-                                        
-                            if (game != null) {
-                                setText(null);
-                                setGraphic(game);
-                                for (GameController juego : gList) {
-                                    if(juego.getView().equals(game) && juego.getWin()){
-                                        getGraphic().setId("wonGame");
-                                        break;
-                                    }
-                                    else if(juego.getView().equals(game)){
-                                        getGraphic().setId("lostGame");
-                                        break;
-                                    }
-                                }
-                                    setBorder(Border.EMPTY);
-                                    setStyle("-fx-background-color:black;");
-                                    setPadding(new Insets(5, 5, 5, 5));
-                                    setWidth(game.getWidth());
-                                    setHeight(game.getHeight());
-                                    setMinHeight(game.getMinHeight());
-                                    setMinWidth(game.getMinWidth());
-                                    setAlignment(Pos.CENTER);
-                            }
-                            else {
-                                    setText("null");
-                                    setGraphic(null);
-                            }
-                        }
-                    };
-                }
-                            
-            });
-            return null;
-            }
-            };        
-            new Thread(task).run();;
+			ObservableList<PieChart.Data> piechartData = FXCollections.observableArrayList(
+					new PieChart.Data("LOSERATE", getWinrate(rankedEntry.getLosses(), rankedEntry.getWins())),
+					new PieChart.Data("a", 0), new PieChart.Data("a", 0),
+					new PieChart.Data("WINRATE", getWinrate(rankedEntry.getWins(), rankedEntry.getLosses())),
+					new PieChart.Data("a", 0), new PieChart.Data("a", 0));
 
-            App.stage.setWidth(view.getPrefWidth()+40);
-            App.stage.setHeight(view.getPrefHeight());
-            App.stage.setMinWidth(view.getMinWidth()+40);
-            App.stage.setMinHeight(view.getMinHeight()+35);
-            }
-        
-    @FXML private void calcOnClick(){
-        if(Objects.isNull(App.calcScene))
-        App.calcScene = new Scene(new CalcController(summoner,rankedEntry).getView());
-        else if(!this.summoner.equals(CalcController.getSummoner()))
-            App.calcScene = new Scene(new CalcController(summoner, rankedEntry).getView());
-        if(Objects.isNull(App.statScene))
-        App.statScene = new Scene(this.getView());
-        App.stage.setScene(App.calcScene);
-        App.stage.setWidth(655);
-        App.stage.setHeight(550);
-        App.stage.setResizable(false);
-        App.stage.show();
-    }
-    
-    @FXML private void loginOnClick(){
-        App.stage.setScene(App.loginScene);
-        App.stage.setWidth(800);
-        App.stage.setHeight(576);
-        App.stage.setResizable(false);
-        App.stage.show();
-    }
+			winrateChart.setData(piechartData);
+			winrateChart.setLegendVisible(false);
 
-    @FXML public static void pdfOnClick() throws JRException, IOException{
-        
-        
-        JasperReport report = JasperCompileManager.compileReport(StatController.class.getResourceAsStream("/reports/gamesReport.jrfxml"));
-        System.out.println("clicked");
-        Map<String, Object> parameters = new HashMap<String, Object>();
+			winrateLabel.setText(String.format("%sW %sL %.2f%%W/R", rankedEntry.getWins(), rankedEntry.getLosses(),
+					getWinrate(rankedEntry.getWins(), rankedEntry.getLosses())));
 
-        parameters.put("sumName", App.summoner.getName());
-        parameters.put("sumLvL", App.summoner.getSummonerLevel());
-        parameters.put("sumElo", StatController.eloValue);
+		} else {
 
-        JasperPrint print = JasperFillManager.fillReport(report, parameters);
+			winrateLabel.setText("");
+			eloLabel.setText("UNRANKED");
 
-        JasperExportManager.exportReportToPdfFile(print, "pdf/gamePDF.pdf");
+		}
 
-        java.awt.Desktop.getDesktop().open(new File("pdf/gamePDF.pdf"));
-    }
+		Task<Void> task = new Task<Void>() {
+			@Override
+			protected Void call() throws Exception {
 
+				ArrayList<GridPane> partidas = new ArrayList<>();
+				gList = new ArrayList<>();
 
+				try {
 
-    private static double getWinrate(int wins, int losses){
-        int partidas = wins + losses;
-        double vic = (double)wins/partidas;
-        return vic*100;
-    }
+					MatchService mService = new MatchService(App.API_KEY, region);
+
+					ArrayList<String> gameIds = mService.getGames(summoner.getPuuid(), App.API_KEY);
+
+					for (String s : gameIds) {
+						GameController g = new GameController(mService.getGame(s));
+						if (g.getWin())
+							g.getView().setId("gameWon");
+						else
+							g.getView().setId("gameLost");
+						gList.add(g);
+						partidas.add(g.getView());
+					}
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+
+				ObservableList<GridPane> partidasToList = FXCollections.observableArrayList(partidas);
+				gamesView.getSelectionModel().setSelectionMode(null);
+				gamesView.setItems(partidasToList);
+				gamesView.setPadding(new Insets(0, 0, 0, 0));
+				gamesView.setFocusModel(null);
+				gamesView.setBorder(Border.EMPTY);
+				gamesView.setFocusTraversable(false);
+				gamesView.setCellFactory(new Callback<ListView<GridPane>, ListCell<GridPane>>() {
+
+					@Override
+					public ListCell<GridPane> call(ListView<GridPane> param) {
+						return new ListCell<>() {
+							@Override
+							public void updateItem(GridPane game, boolean empty) {
+								super.updateItem(game, empty);
+
+								if (game != null) {
+									setText(null);
+									setGraphic(game);
+									for (GameController juego : gList) {
+										if (juego.getView().equals(game) && juego.getWin()) {
+											getGraphic().setId("wonGame");
+											break;
+										} else if (juego.getView().equals(game)) {
+											getGraphic().setId("lostGame");
+											break;
+										}
+									}
+									setBorder(Border.EMPTY);
+									setStyle("-fx-background-color:black;");
+									setPadding(new Insets(5, 5, 5, 5));
+									setWidth(game.getWidth());
+									setHeight(game.getHeight());
+									setMinHeight(game.getMinHeight());
+									setMinWidth(game.getMinWidth());
+									setAlignment(Pos.CENTER);
+								} else {
+									setText("null");
+									setGraphic(null);
+								}
+							}
+						};
+					}
+
+				});
+				return null;
+			}
+		};
+		new Thread(task).run();
+		;
+
+		App.stage.setWidth(view.getPrefWidth() + 40);
+		App.stage.setHeight(view.getPrefHeight());
+		App.stage.setMinWidth(view.getMinWidth() + 40);
+		App.stage.setMinHeight(view.getMinHeight() + 35);
+	}
+
+	@FXML
+	private void calcOnClick() {
+		if (Objects.isNull(App.calcScene))
+			App.calcScene = new Scene(new CalcController(summoner, rankedEntry).getView());
+		else if (!this.summoner.equals(CalcController.getSummoner()))
+			App.calcScene = new Scene(new CalcController(summoner, rankedEntry).getView());
+		if (Objects.isNull(App.statScene))
+			App.statScene = new Scene(this.getView());
+		App.stage.setScene(App.calcScene);
+		App.stage.setWidth(655);
+		App.stage.setHeight(550);
+		App.stage.setResizable(false);
+		App.stage.show();
+	}
+
+	@FXML
+	private void loginOnClick() {
+		App.stage.setScene(App.loginScene);
+		App.stage.setWidth(800);
+		App.stage.setHeight(576);
+		App.stage.setResizable(false);
+		App.stage.show();
+	}
+
+	@FXML
+	public void pdfOnClick() throws JRException, IOException {
+
+//		System.out.println(getClass().getResourceAsStream("/reports/gamesReport.jrxml"));
+		
+		JasperReport report = JasperCompileManager.compileReport(getClass().getResourceAsStream("/reports/gamesReport.jrxml"));
+		System.out.println("clicked");
+		Map<String, Object> parameters = new HashMap<String, Object>();
+
+		parameters.put("sumName", App.summoner.getName());
+		parameters.put("sumLvL", App.summoner.getSummonerLevel());
+		parameters.put("sumElo", StatController.eloValue);
+
+		JasperPrint print = JasperFillManager.fillReport(report, parameters);
+
+		File pdf = new File("pdf");
+		if (!pdf.exists()) {
+			pdf.mkdir();
+		}
+		
+		JasperExportManager.exportReportToPdfFile(print, "pdf/gamePDF.pdf");
+
+		java.awt.Desktop.getDesktop().open(new File("pdf/gamePDF.pdf"));
+	}
+
+	private static double getWinrate(int wins, int losses) {
+		int partidas = wins + losses;
+		double vic = (double) wins / partidas;
+		return vic * 100;
+	}
 
 }
